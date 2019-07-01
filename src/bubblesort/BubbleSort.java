1ab54798d81faf0b530f217289f9c2cf8c9e31f5
Natalie Sandford <nataliesandford@rentalcars.com>
2019-07-01 10:50:11 +0100

    Refactored + split out some logic into methods

--- src/bubblesort/BubbleSort.java
+++ src/bubblesort/BubbleSort.java
@@ -8,42 +8,59 @@ public class BubbleSort {
     }
 
 //    start bubble sort
-//        (print out unsorted array)
 //        loop through all numbers in the range 0 to the length of the array
 //            loop through all numbers in the range 1 to the length of the array
-//                check if the last number in the current array position is greater than the current value.
-//                    store the smaller number in a temporary variable
-//                    put the larger number in the position of the smaller number
-//                    put the temporary value where the larger number was
+//                check if the last number in the array is greater than the current number
+//                    swap the two numbers
 //                end if
-//                (print out array)
 //            end loop
-//            (print out array)
 //        end loop
-//        (print out sorted array)
 //    end bubble sort
 
     private void sort(int[] arrayToSort) {
 
-        System.out.println("Sorting array: " + printArray(arrayToSort));
+        System.out.println("Sorting array: " + printArray(arrayToSort) + '\n');
+        // loop through all numbers in the range 0 to the length of the array
         for (int i = 1; i < arrayToSort.length - 1; i++) {
+            // loop through all numbers in the range 1 to the length of the array
             for (int j = 0; j < arrayToSort.length - i; j++) {
                 int current = j;
                 int next = j + 1;
                 boolean swap = false;
+                // check if the last number in the array is greater than the current number
                 if (arrayToSort[current] > arrayToSort[next]) {
+                    // swap the two numbers
                     int temp = arrayToSort[current];
                     arrayToSort[current] = arrayToSort[next];
                     arrayToSort[next] = temp;
                     swap = true;
                 }
-                System.out.println("Pass " + i + '.' + (current + 1) + ": " + printArray(arrayToSort));
+                printPass(i, current, next, arrayToSort, swap);
                 printIndicator(current, swap);
             }
-            System.out.println("Array at end of pass " + i + ": " + printArray(arrayToSort));
+            printUpdatedArray(arrayToSort, i);
             System.out.println("\n");
         }
-        System.out.println("Sorted array: " + printArray(arrayToSort));
+        printSortedArray("Sorted array: " + printArray(arrayToSort));
+    }
+
+    private void printSortedArray(String s) {
+        System.out.println(s);
+    }
+
+    private void printUpdatedArray(int[] arrayToSort, int i) {
+        System.out.println("Array at end of pass " + i + ": " + printArray(arrayToSort));
+    }
+
+    private void printPass(int i, int current, int next, int[] arrayToSort, boolean swap) {
+        System.out.println("Pass " + i + ", move " + (current + 1) + ": " + printArray(arrayToSort) + " " + printSwap(swap, arrayToSort[next], arrayToSort[current]));
+    }
+
+    private String printSwap(boolean swap, int lhs, int rhs) {
+        if (swap) {
+            return lhs + " swapped with " + rhs;
+        }
+        return "no swap";
     }
 
     private String printArray(int[] arrayToPrint) {
@@ -59,14 +76,12 @@ public class BubbleSort {
     }
 
     private void printIndicator(int index, boolean swap) {
-        String print = "           ";
+        String print = "                 ";
         for (int i = 0; i < index; i++) {
             print += "   ";
         }
-        print += "^";
-        if (swap) {
-            print += "--^";
-        }
+        if (swap) print += "^--^";
+        else print += "x--x";
         System.out.println(print);
     }
 }
